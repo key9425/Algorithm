109fcd67b21798cb8c2a7e3c2824284d000aa640
key9425 <kimeunyoung-0425@naver.com>
2025-10-04 23:07:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 15.66 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,3 @@
 function solution(myStr) {
-  const answer = myStr.split(/[abc]/).filter((str) => str !== "");
-  return answer.length === 0 ? ["EMPTY"] : answer;
+  return myStr.match(/[^abc]+/g) || ["EMPTY"];
 }
