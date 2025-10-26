9aef184891b21aea10c617c73f42074a2cee120b
key9425 <kimeunyoung-0425@naver.com>
2025-10-26 22:50:08 +0900

    [Gold IV] Title: 행성 연결, Time: 436 ms, Memory: 74908 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -1,24 +1,35 @@
 import sys
 import heapq
+
 input = sys.stdin.readline
+INF = sys.maxsize
 
 N = int(input())
 adj = []
-for i in range(N):
-    adj.append([(int(v), j) for j, v in enumerate(input().split()) if i != j])
+for _ in range(N):
+    adj.append(list(map(int, input().split())))
 
 heap = [(0, 0)]
 chk = [False] * N
+min_cost = [INF] * N
 cost = 0
+node_count = 0
 
-while heap:
+while heap and node_count < N:
     ew, en = heapq.heappop(heap)
-    if chk[en] == False:
-        chk[en] = True
-        cost += ew
-        for next in adj[en]:
-            if chk[next[1]] == False:
-                heapq.heappush(heap, next)
+
+    if chk[en]:
+        continue
+
+    chk[en] = True
+    cost += ew
+    node_count += 1
+
+    for v in range(N):
+        nw = adj[en][v]
+        if chk[v] == False and nw < min_cost[v]:
+            min_cost[v] = nw
+            heapq.heappush(heap, (nw, v))
 
 
 print(cost)
